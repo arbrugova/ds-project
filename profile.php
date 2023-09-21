ce5753f7bc9baac4b6c2a82f2e9a866ee1c9eb28
Arbi <arb.rugova43@gmail.com>
2023-09-21 16:20:48 +0200

    Added seller data (education info)

--- profile.php
+++ profile.php
@@ -15,7 +15,12 @@ function getJobListingsByUserId($user_id, $db) {
 }
 
 function getUserData($user_id, $db) {
-    $query = $db->prepare('SELECT u.username, u.email, p.bio FROM users AS u LEFT JOIN seller AS p ON u.id = p.id WHERE u.id = :user_id');
+    $query = $db->prepare('
+        SELECT u.username, u.email, s.position, s.education, s.degree, s.rate, s.bio
+        FROM users AS u
+        LEFT JOIN seller AS s ON u.id = s.id
+        WHERE u.id = :user_id
+    ');
     $query->bindParam(':user_id', $user_id, PDO::PARAM_INT);
 
     if ($query->execute()) {
@@ -39,11 +44,11 @@ if (isset($_GET['user_id'])) {
     if ($profile_user_id == $user_id) {
         $is_own_profile = true;
         $profile_data = getUserData($user_id, $db);
-        $job_listings = getJobListingsByUserId($user_id, $db); // Fetch job listings for the user's own profile
+        $job_listings = getJobListingsByUserId($user_id, $db); 
     } else {
         $is_own_profile = false;
         $profile_data = getUserData($profile_user_id, $db);
-        $job_listings = getJobListingsByUserId($profile_user_id, $db); // Fetch job listings for the other user's profile
+        $job_listings = getJobListingsByUserId($profile_user_id, $db); 
     }
 
     if (!$profile_data) {
@@ -66,35 +71,41 @@ if (isset($_GET['user_id'])) {
 </head>
 <body>
 <nav class="navbar">
-  <a href="index.php" class="navbar-brand">Kosova freelance</a>
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a href="explore.php" class="nav-link">Explore</a>
-    </li>
-    <li class="nav-item">
-      <a href="seller.php" class="nav-link">Become a seller</a>
-    </li>
-    <?php if(isset($_SESSION['user_id'])){?>
-      <li class="nav-item">
-      <a href="profile.php?user_id=<?php echo $_SESSION['user_id']?>" class="nav-link">Profile</a>
-    </li>
-    <li class="nav-item">
-      <button>
-      <a href="logout.php" class="nav-link">Log out</a>
-      </button>
-    </li>
-    <?php }else { ?>
-    <li class="nav-item">
-      <a href="login.php" class="nav-link">Sign in</a>
-    </li>
-    <li class="nav-item">
-      <button>
-      <a href="signup.php" class="nav-link">Join</a>
-    </button>
-    </li>
-    <?php } ?>
-  </ul>
-</nav>
+            <a href="index.php" class="navbar-brand">Kosova freelance</a>
+            <ul class="navbar-nav">
+                <li>
+                <a href="explore.php" class="nav-link">Explore</a>
+                </li>
+                <?php if(isset($_SESSION['seller-id'])){ ?>
+                    <li>
+                        <button style="background-color: #007bff; width: 180px; padding-right:10px;">
+                            <a href="user_dashboard.php" class="nav-link">User Dashboard</a>
+                        </button>
+                    </li>
+                <?php } else { ?>
+                    <li>
+                        <a href="seller.php" class="nav-link">Become a seller</a>
+                    </li>
+                <?php } ?>
+
+                <?php if(isset($_SESSION['user_id'])){ ?>
+                    <li>
+                        <button>
+                            <a href="logout.php" class="nav-link">Log out</a>
+                        </button>
+                    </li>
+                <?php } else { ?>
+                    <li>
+                        <a href="login.php" class="nav-link">Sign in</a>
+                    </li>
+                    <li>
+                        <button>
+                         <a href="signup.php" class="nav-link">Join</a>
+                        </button>
+                    </li>
+                <?php } ?>
+            </ul>
+        </nav>
 <section class="profile-section">
     <div class="profile-container">
         <div class="profile-picture"></div>
@@ -107,6 +118,11 @@ if (isset($_GET['user_id'])) {
         <?php if ($is_own_profile) { ?>
             <a href="editProfile.php?user_id=<?php echo $_SESSION['user_id']; ?>" class="edit-button">Edit Profile</a>
         <?php } ?>
+        <div>
+            <p><?php echo $profile_data['position'];?></p>
+            <p>Education: <?php echo $profile_data['education'];?></p>
+            <?php echo $profile_data['degree'];?>
+        </div>
         <div class="social-media">
             <a href="#" target="_blank">Facebook</a>
             <a href="#" target="_blank">Twitter</a>
