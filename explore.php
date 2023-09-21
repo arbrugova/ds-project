701334cfbe4285fe4271c42b7b3a259cc1d33b1d
Arbi <arb.rugova43@gmail.com>
2023-09-21 16:23:58 +0200

    Added some styling

--- explore.php
+++ explore.php
@@ -3,62 +3,94 @@ session_start();
 
 require_once "db_conn.php";
 
-if (!isset($_SESSION['user_id'])) { ?>
-  <button><a href="login.php">Please log in</a></button>
-  <?php }else{
-    
-    $query = $db->prepare('SELECT * FROM job_listing');
-     
-    $query->execute();
-    if ($query->execute()) {
-    
-    
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+$query = $db->prepare('SELECT * FROM job_listing');
+
+if ($query->execute()) {
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
-    
-    if ($result) { ?>
-      <table>
-    <tr>
-        <th>Number</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Location</th>
-        <th>Created</th>
-        <th>Ends in</th>
-        <th>Salary ($)</th>
-        <th>Rate</th>
-        <th>Action</th>
-    </tr>
-    <?php
-    foreach($result as $item){
-    echo '<tr>';
-    echo '<td>' . $item['job_id'] . '</td>';
-    echo '<td>' . $item['title'] . '</td>';
-    echo '<td>' . $item['description'] . '</td>';
-    echo '<td>' . $item['location'] . '</td>';
-    echo '<td>' . $item['created_at'] . '</td>';
-    echo '<td>' . $item['deadline'] . '</td>';
-    echo '<td>' . $item['salary'] . '$</td>';
-    echo '<td>' . $item['status'] . '</td>';
-    echo '<td><a href="application.php?job_id=' . $item['job_id'] . '">Apply</a></td>';
-    echo '</tr>';
+
+    if ($result) {
+        $jobListings = $result;
     }
-    ?>
-</table>
-
-
-
-    <?php } else {
-      echo '<table>
-        <tr>
-          <td colspan="5">No data found</td>
-        </tr>
-      </table>';
-    }
-    }else {
-        echo "Query execution failed: " . $query->errorInfo()[2];
-    }
-
-  }
-
-
+} else {
+    echo "Query execution failed: " . $query->errorInfo()[2];
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Job Listings</title>
+</head>
+<body>
+  <section class="explore">
+    <!-----Navbar----->
+    <nav class="navbar">
+      <a href="index.php" class="navbar-brand">Kosova freelance</a>
+      <ul class="navbar-nav">
+          <li>
+          <a href="explore.php" class="nav-link">Explore</a>
+          </li>
+          <?php if(isset($_SESSION['seller-id'])){ ?>
+              <li>
+                  <button style="background-color: #007bff; width: 180px; padding-right:10px;">
+                      <a href="user_dashboard.php" class="nav-link">User Dashboard</a>
+                  </button>
+              </li>
+          <?php } else { ?>
+              <li>
+                  <a href="seller.php" class="nav-link">Become a seller</a>
+              </li>
+          <?php } ?>
+
+          <?php if(isset($_SESSION['user_id'])){ ?>
+              <li>
+                  <button>
+                      <a href="logout.php" class="nav-link">Log out</a>
+                  </button>
+              </li>
+          <?php } else { ?>
+              <li>
+                  <a href="login.php" class="nav-link">Sign in</a>
+              </li>
+              <li>
+                  <button>
+                  <a href="signup.php" class="nav-link">Join</a>
+                  </button>
+              </li>
+          <?php } ?>
+      </ul>
+    </nav>
+
+    <div class="job-listings">
+      <ul class="job-list">
+          <h2>Job Listings</h2>
+          <p>Total listings: <?php echo count($jobListings);?></p>
+            <?php if (!empty($jobListings)) { ?>
+                <?php foreach ($jobListings as $item) { ?>
+                    <li class="job-item">
+                        <h3><?php echo $item['title']; ?></h3>
+                        <p class="description"><?php echo $item['description']; ?></p>
+                        <p class="category">Location: <?php echo $item['location']; ?></p>
+                        <p class="category">Created: <?php echo $item['created_at']; ?></p>
+                        <p class="category">Deadline: <?php echo $item['deadline']; ?></p>
+                        <p class="status"><?php echo $item['status']; ?></p>
+                        <button class="apply-btn"><a href="application.php?job_id=<?php echo $item['job_id']; ?>">Apply</a></button>
+                        <p class="category">Salary: <?php echo $item['salary']; ?></p>
+                    </li>
+                <?php } ?>
+            <?php } else { ?>
+                <p class="no-listings">No job listings found.</p>
+            <?php } ?>
+        </ul>
+    </div>
+  </section>
+</body>
+</html>
