1516f159d1d6f68d8bd6513253122344a51a0646
Arbi <arb.rugova43@gmail.com>
2023-09-28 17:46:26 +0200

    Hiring listing logic

---
+++ hiringListingLogic.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+require_once "db_conn.php";
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $category = $_POST['category'];
+    $location = $_POST['location'];
+    $deadline = $_POST['deadline'];
+    $salary = $_POST['salary'];
+
+    $creator_id = $_SESSION['user_id'];
+
+    $sql = "INSERT INTO hiring_listing (creator_id, title, description, category, location, created_at, deadline, salary, status)
+            VALUES (:creator_id, :title, :description, :category, :location, NOW(), :deadline, :salary, 'Open')";
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(':creator_id', $creator_id);
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':category', $category);
+    $stmt->bindParam(':location', $location);
+    $stmt->bindParam(':deadline', $deadline);
+    $stmt->bindParam(':salary', $salary);
+
+    try {
+        $stmt->execute();
+        echo "Job listing created successfully!";
+        header("Location: user_dashboard.php");
+    } catch (PDOException $e) {
+        echo "Error creating job listing: " . $e->getMessage();
+    }
+}
+?>
